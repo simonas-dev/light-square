00ca6fe2d9748dc3c01593ea3fffecd579f5c4cd
Simonas Sankauskas <simonas.sank@gmail.com>
2019-04-18 12:32:59 +0300

    App Context

---
+++ src/AppContext.cpp
@@ -0,0 +1,8 @@
+//
+//  AppContext.cpp
+//  siLedBox
+//
+//  Created by Simonas Sankauskas on 4/18/19.
+//
+
+#include "AppContext.hpp"
---
+++ src/AppContext.hpp
@@ -0,0 +1,21 @@
+//
+//  AppContext.hpp
+//  siLedBox
+//
+//  Created by Simonas Sankauskas on 4/18/19.
+//
+
+#ifndef AppContext_hpp
+#define AppContext_hpp
+
+#include <stdio.h>
+#include <AudioClient.hpp>
+#include <LedClient.hpp>
+
+#endif /* AppContext_hpp */
+
+class AppContext {
+    
+public:
+    
+};
